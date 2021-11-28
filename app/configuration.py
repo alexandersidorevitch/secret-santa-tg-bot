86643d7798f1594382e00078d97f12016a6e2702
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2021-11-28 20:12:53 +0300

    Add admin_username constant. Add memory storage for dispatcher.

---
+++ app/configuration.py
@@ -0,0 +1,28 @@
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import BotCommand
+from dotenv import load_dotenv
+from loguru import logger
+from app.participant_io import ParticipantIO
+
+load_dotenv()
+
+token = os.getenv('BOT_TOKEN')
+ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
+bot = Bot(token=token)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+logger.add('logging/file.log', format='{time} {level} {message}', rotation='1 MB', compression='zip',
+           level='WARNING')
+
+participant_io = ParticipantIO()
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="/start", description="Запустить бота"),
+        BotCommand(command="/view_my_wishes", description="Вывести мои пожелания"),
+    ]
+    await bot.set_my_commands(commands)
