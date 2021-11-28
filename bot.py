cdf08e30f287fa8d9ddab9fe49730658d7bbc99e
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2021-11-28 21:08:28 +0300

    Add deletion for wishes.

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ import asyncio
 from random import randint, shuffle
 
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ContentTypes, InlineKeyboardButton
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 import app.configuration as configuration
 from app.factory import ParticipantFactory
-from app.wish_info import *
+from app.wish_info import TextWishInfo, LinkWishInfo, PhotoWishInfo
 
 is_admin = lambda message: message.from_user.username == configuration.ADMIN_USERNAME
 
@@ -29,9 +30,14 @@ async def start_bot_command(message: types.Message):
     buttons = [
         InlineKeyboardButton(text='Добавить пожелание', callback_data='add_wish'),
     ]
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(*buttons)
+    keyboard = get_inline_keyboard(buttons, row_width=1)
     await message.answer('[Приведственный текст]', reply_markup=keyboard)
+
+
+def get_inline_keyboard(buttons, **kwargs):
+    keyboard = types.InlineKeyboardMarkup(**kwargs)
+    keyboard.add(*buttons)
+    return keyboard
 
 
 @configuration.dp.message_handler(commands=['view_my_wishes'])
@@ -41,13 +47,38 @@ async def view_wishes_command(message: types.Message):
 
 @configuration.dp.message_handler(is_admin, commands=['celebrate'])
 async def celebrate(message: types.Message):
+    await start_mailing_async(message)
+
+
+@configuration.dp.message_handler(is_admin, commands=['delete_wish'])
+async def delete_wish_command(message: types.Message):
+    participant = ParticipantFactory.get_participant(message.from_user)
+    buttons = [
+        InlineKeyboardButton(f'{i + 1}. {wish}', callback_data=f'delete_button_{i}')
+        for i, wish in enumerate(participant.wishes)
+    ]
+    keyboard = get_inline_keyboard(buttons, row_width=2)
+    await message.answer('Выберите что удалить', reply_markup=keyboard)
+
+
+@configuration.dp.callback_query_handler(Text(startswith="delete_button_"))
+async def delete_wish_button(call: types.CallbackQuery):
+    *_, wish_index_to_delete = call.data.split('_')
+    wish_index_to_delete = int(wish_index_to_delete)
+    participant = ParticipantFactory.get_participant(call.from_user)
+    del participant.wishes[wish_index_to_delete]
+    configuration.participant_io.save(participant)
+    await call.message.answer('OK')
+
+
+async def start_mailing_async(message):
     shuffled_participants = get_shuffle_participants()
     participants_length = len(shuffled_participants)
     shift = randint(0, participants_length - 1)
-
     await message.answer('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII')
     for i, participant in enumerate(shuffled_participants):
         participant.receiver = shuffled_participants[(i + shift) % participants_length]
+        await message.answer(f'Вы дарите {participant.receiver.name} cыылка на него @{participant.receiver.user_name}')
         for wish in participant.receiver.wishes:
             await wish.send_to_async(participant.id)
 
@@ -62,7 +93,7 @@ async def view_participant_wishes(message):
     participant = ParticipantFactory.get_participant(message.from_user)
     if participant.wishes:
         for wish in participant.wishes:
-            await wish.send_to_async(message, format='Вы хотели бы {}')
+            await wish.send_to_async(participant.id, format='Вы хотели бы {}')
     else:
         await message.answer('У вас не желашек((')
 
@@ -74,8 +105,7 @@ async def add_present(call: types.CallbackQuery):
         InlineKeyboardButton(text='Добавить фото подарка', callback_data='add_photo_wish_button'),
         InlineKeyboardButton(text='Добавить ссылку на подарок', callback_data='add_link_wish_button')
     ]
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    keyboard.add(*buttons)
+    keyboard = get_inline_keyboard(buttons, row_width=1)
 
     await call.message.answer('Выберите что хотите добавить', reply_markup=keyboard)
 
@@ -96,7 +126,7 @@ async def add_text_wish(message: types.Message, state: FSMContext):
     await state.finish()
     await message.answer('Хорош!!')
 
-ContentTypes.
+
 @configuration.dp.message_handler(state=TextWishState.wait_for_wish, content_types=ContentTypes.ANY)
 async def add_text_wish(message: types.Message, state: FSMContext):
     await message.answer('Введите текст((')
