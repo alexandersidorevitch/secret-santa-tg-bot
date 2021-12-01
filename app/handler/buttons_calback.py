313c0dd1e070b668f4edc9e22d5860c79de3121b
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2021-12-01 19:21:20 +0300

    Edit callback verify method

--- app/handler/buttons_calback.py
+++ app/handler/buttons_calback.py
@@ -1,18 +1,18 @@
 import app.configuration as configuration
 from aiogram import types
 from app.factory import ParticipantFactory
-
+from app.functions import verify_inline_button_callback
 
 async def delete_wish_button(call: types.CallbackQuery):
-    *_, wish_index_to_delete, wishes_len = call.data.split('_')
+    *_, wish_index_to_delete, verify_hash = call.data.split('_')
     wish_index_to_delete = int(wish_index_to_delete)
     participant = ParticipantFactory.get_participant(call.from_user, call.message.chat.id)
-    if len(participant.wishes) < int(wishes_len):
-        await call.message.answer('Я не могу его удалить, т.к. данный подарок мог быть удаленен.\n'
-                                  r'Введите /delete_wish, чтобы удалить подарок')
-    else:
+    if verify_inline_button_callback(participant, verify_hash):
         wish_to_delete = participant.wishes[wish_index_to_delete]
         participant.wishes[wish_index_to_delete].delete()
         configuration.participant_io.save(participant)
         await call.message.answer('Убрал из вашего списка это пожелание')
         await wish_to_delete.send_to_async(call.message.chat.id)
+    else:
+        await call.message.answer('Я не могу его удалить, т.к. данный подарок мог быть удаленен.\n'
+                                  r'Введите /delete_wish, чтобы удалить подарок')
