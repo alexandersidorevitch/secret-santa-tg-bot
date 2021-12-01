2a84d3868af112c0a47b95e049c50ac290632575
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2021-12-01 18:29:11 +0300

    Edit message parse mode. Set logger level to info.

--- app/configuration.py
+++ app/configuration.py
@@ -1,15 +1,15 @@
-from aiogram import Bot, Dispatcher
+import app.settings as settings
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import BotCommand
 from app.participant_io import ParticipantIO
 from loguru import logger
-import app.settings as settings
 
-bot = Bot(token=settings.TOKEN)
+bot = Bot(token=settings.TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 logger.add('logging/file.log', format='{time} {level} {message}', rotation='1 MB', compression='zip',
-           level='WARNING')
+           level='INFO')
 
 participant_io = ParticipantIO()
 
