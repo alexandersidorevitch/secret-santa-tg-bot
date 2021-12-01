b299b257bb115f9d21855f88d5dd4f70d88ce396
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2021-12-01 18:28:36 +0300

    Edit search attribute

--- app/factory.py
+++ app/factory.py
@@ -1,15 +1,15 @@
 from aiogram import types
-from app.participant import Participant
+from app.entities.participant import Participant
 
 import app.configuration as configuration
 
 
 class ParticipantFactory:
     @staticmethod
-    def get_participant(user_info) -> Participant:
-        user_id = user_info.id
-        if user_id not in configuration.participant_io.participants:
+    def get_participant(user_info, chat_id) -> Participant:
+        username = user_info.username
+        if username not in configuration.participant_io.participants:
             configuration.participant_io.save(
-                Participant(user_info.full_name, user_id, user_info.username)
+                Participant(user_info.full_name, username, chat_id)
             )
-        return configuration.participant_io.participants[user_id]
+        return configuration.participant_io.participants[username]
